fc72a3b88ed1cf780458f4fc45955b6ca9fa4bc1
ianyimi <anyiminene@gmail.com>
2023-04-19 23:12:24 -0700

    scrolling clouds w leva controls for now

--- src/components/canvas/About/index.tsx
+++ src/components/canvas/About/index.tsx
@@ -1,11 +1,12 @@
 import { useThree } from '@react-three/fiber';
 import CloudOne from './Models/CloudOne';
+import { useControls } from 'leva';
 
 export default function About() {
   const { viewport } = useThree();
   return (
     <group position={[0, -viewport.height / 3, 3.5]}>
-      <CloudOne scale={0.5} position={[1, 0.5, 0]} />
+      <CloudOne position={[0, 0, 0]} />
     </group>
   );
 }
---
+++ src/utils/canvas.ts
@@ -0,0 +1,28 @@
+import { Vector3 } from 'three';
+
+function randomPositionVector(constraint: number | [number, number, number]) {
+  if (typeof constraint === 'number') {
+    return new Vector3(
+      constraint / 2 - Math.random() * constraint,
+      constraint / 2 - Math.random() * constraint,
+      constraint / 2 - Math.random() * constraint
+    );
+  } else {
+    return new Vector3(
+      constraint[0] / 2 - Math.random() * constraint[0],
+      constraint[1] / 2 - Math.random() * constraint[1],
+      constraint[2] / 2 - Math.random() * constraint[2]
+    );
+  }
+}
+
+export function generateInstancedModelData(constraint: number | [number, number, number], scaleVariance?: number) {
+  const data = Array.from({ length: 1000 }, () => {
+    return {
+      position: randomPositionVector(constraint),
+      // color: [Math.random(), Math.random(), Math.random(), 1],
+      scale: scaleVariance ? Math.random() * (0.5 + scaleVariance) + 0.5 : 1,
+    };
+  });
+  return data;
+}
