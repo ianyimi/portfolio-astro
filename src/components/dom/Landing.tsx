dc25c79f124c57ae4bfdbcc2f4d35012ae5fbe9e
ianyimi <anyiminene@gmail.com>
2023-05-01 15:51:17 -0700

    tailwindui start

--- src/components/dom/Landing.tsx
+++ src/components/dom/Landing.tsx
@@ -1,37 +1,139 @@
 import { useDarkMode } from 'usehooks-ts';
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'About', href: '#about-section' },
+  { name: 'Work', href: '#work-section' },
+  { name: 'Contact', href: '#contact-section' },
+]
 
 export default function Landing() {
   const { isDarkMode } = useDarkMode();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
-    <div id="landing-section" className="grid place-items-center min-h-screen min-w-screen overflow-hidden border-none">
-      <div className="fixed top-10 right-10">
-        <input
-          id="theme-toggle"
-          type="checkbox"
-          data-toggle-theme="spaceCadetRed,floralWhiteFlame"
-          className="toggle"
-          data-act-class="ACTIVECLASS"
-          defaultChecked={isDarkMode}
+    <div id="landing-section" className="min-h-screen min-w-screen">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden lg:flex lg:gap-x-12">
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <div className="fixed top-10 right-10">
+              <input
+                id="theme-toggle"
+                type="checkbox"
+                data-toggle-theme="spaceCadetRed,floralWhiteFlame"
+                className="toggle"
+                data-act-class="ACTIVECLASS"
+                defaultChecked={isDarkMode}
+              />
+            </div>
+          </div>
+        </nav>
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt=""
+                />
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+      <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+        <div
+          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right sm:-mr-80 lg:-mr-96"
+          aria-hidden="true"
         />
-      </div>
-      <div className="hero-content text-center">
-        <div className="max-w-md mt-[350px]">
-          <h1 id="header" className="text-5xl font-bold">
-            Isaiah Anyimi
-          </h1>
-          <p className="py-6">
-            A front-end engineer creating fast, responsive, and accessible websites using modern web technologies. I
-            enjoy building stunning user interfaces that enhance user experience, promote engagement, and drive
-            conversions.
-          </p>
-          <a
-            className="btn btn-primary sm:btn-sm md:btn-md lg:btn-lg rounded-full"
-            href="https://www.instagram.com/yonkozay/"
-            target="blank"
-          >
-            Contact
-          </a>
+        <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 ">
+            <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+              Isaiah Anyimi
+            </h1>
+            <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+              <p className="text-lg leading-8 text-gray-600">
+                A front-end engineer creating fast, responsive, and accessible websites using modern web technologies. I
+                enjoy building stunning user interfaces that enhance user experience, promote engagement, and drive
+                conversions.
+              </p>
+              <div className="mt-10 flex items-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get started
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+        {/* <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" /> */}
       </div>
     </div>
   );
